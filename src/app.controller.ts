81e1548582eb584712920105448d081b6b836616
RenildoPaesJob <renildopaesjoob@gmail.com>
2024-05-10 18:12:44 -0300

    start service email

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,31 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { MailerService } from '@nestjs-modules/mailer';
+import { Controller } from '@nestjs/common';
 import { EventPattern } from '@nestjs/microservices';
+
+type NotificationDTO = {
+	email: string,
+	startAt: Date,
+	endAt: Date,
+	name: string,
+	title: string,
+	description: string
+}
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  constructor(
+		private mailerService: MailerService
+	) {}
 
 	@EventPattern("task_notification")
-	taskNotification(data: any){
+	async taskNotification(data: NotificationDTO){
 		console.log("Recebendo mensagem: ", data);
+
+		await this.mailerService.sendMail({
+			to: data.email,
+			from: "taskmanager@nestjscurso.com.br",
+			text: `Olá ${data.name} você tem atividade ${data.title} que inicia ${data.startAt} e finaliza em ${data.endAt}`
+		})
 	}
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+	imports: [
+		MailerModule.forRoot({
+			transport: {
+				host: "smtp.ethereal.email",
+				port: 587,
+				secure: false,
+				auth: {
+					user: 'avery.schmeler@ethereal.email',
+					pass: '2sR9DKuXQsmmKHYJDB'
+				}
+			}
+		})
+	],
+	controllers: [AppController],
+	providers: [],
 })
-export class AppModule {}
+export class AppModule { }
